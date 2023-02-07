dc9bf8c9e118d5528ab671a8983c844d1fd25f0f
Ansell Gabriel Del Ayre <ansellgabrieldelayre@Ansells-MacBook-Pro.local>
2023-02-07 18:31:49 +0800

    check email sending4

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.hosts << 'https://event.1cisp.coop/'
+  # config.hosts << 'https://event.1cisp.coop/'
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { host: 'https://event.1cisp.coop/' }
   # In the development environment your application's code is reloaded any time
