4302265ff3b93bdd529956ce6bd35ff4949b12f9
Ansell Gabriel Del Ayre <ansellgabrieldelayre@Ansells-MacBook-Pro.local>
2023-02-08 10:09:11 +0800

    check email sending

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -28,7 +28,7 @@ class RegistrationsController < ApplicationController
   def new
     @event_hub = EventHub.find(params[:v])
     @registration = @event_hub.registrations.build
-    set_dummy_register
+    # set_dummy_register
   end
   def set_dummy_register 
     @eh = EventHub.all
@@ -47,7 +47,7 @@ class RegistrationsController < ApplicationController
   def new_modal
       # puts "@@@@@#{params[:v]}"
       @registration = Registration.new
-      set_dummy_register
+      # set_dummy_register
     
   end
   # GET /registrations/1/edit
