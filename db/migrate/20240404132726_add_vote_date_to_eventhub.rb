ac0bac19a725b88fa3725cbd57868bfc36188955
Ansell Gabriel Del Ayre <ansellgabriel@gmail.com>
2024-04-19 14:40:07 +0800

    commented vote_date column in migration

--- db/migrate/20240404132726_add_vote_date_to_eventhub.rb
+++ db/migrate/20240404132726_add_vote_date_to_eventhub.rb
@@ -1,6 +1,6 @@
 class AddVoteDateToEventhub < ActiveRecord::Migration[7.0]
   def change
-    add_column :event_hubs, :vote_date, :datetime
+    # add_column :event_hubs, :vote_date, :datetime
     # add_column :event_hubs, :attend, :boolean
   end
 end
