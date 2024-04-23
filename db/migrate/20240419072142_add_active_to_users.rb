eda58efd74f448ded218ce50a517ebc7437081f5
Ansell Gabriel Del Ayre <ansellgabriel@gmail.com>
2024-04-23 14:32:53 +0800

    create migration for attend_date

--- db/migrate/20240419072142_add_active_to_users.rb
+++ db/migrate/20240419072142_add_active_to_users.rb
@@ -2,6 +2,6 @@ class AddActiveToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :active, :boolean
     add_column :users, :admin, :boolean
-    add_column :registrations, :attend_date, :datetime
+    # add_column :registrations, :attend_date, :datetime
   end
 end
